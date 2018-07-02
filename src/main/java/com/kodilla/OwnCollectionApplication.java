a43161e968101d0da2ba9658c5cba9db1756d357
Foqz <fikskamil@gmail.com>
2018-07-02 11:31:02 +0200

    1 commit

---
+++ src/main/java/com/kodilla/OwnCollectionApplication.java
@@ -0,0 +1,12 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwnCollectionApplication.class, args);
+    }
+}
